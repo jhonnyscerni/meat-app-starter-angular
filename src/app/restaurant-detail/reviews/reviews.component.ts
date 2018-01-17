b09f3074cfa04d33e63702e5e0ef968bd318417f
jhonnyscerni <jhonnyscerni@gmail.com>
2018-01-17 10:09:02 -0300

    Implementando o Componente de Avaliações - Pipes Async e Date

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,4 +1,7 @@
+import { RestaurantsService } from './../../restaurants/restaurants.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'mt-reviews',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  reviews: Observable<any>
+
+  constructor(private restaurantsService: RestaurantsService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.reviews = this.restaurantsService
+    .reviewsOfRestaurant(this.route.parent.snapshot.params['id'])
   }
 
 }
